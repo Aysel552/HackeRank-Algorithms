799ece2b8b5fe773878e02e5feade0dc4a5c3932
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-04-15 19:18:46 +0400

    staircase

---
+++ script/staircase.js
@@ -0,0 +1,77 @@
+// Staircase detail
+
+// This is a staircase of size :
+
+//    #
+//   ##
+//  ###
+// ####
+// Its base and height are both equal to . It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+// Write a program that prints a staircase of size .
+
+// Function Description
+
+// Complete the staircase function in the editor below.
+
+// staircase has the following parameter(s):
+
+// int n: an integer
+// Print
+
+// Print a staircase as described above.
+
+
+
+
+
+
+
+
+
+
+
+
+// Solution:
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+function staircase(n) {
+    // Write your code here
+    for (let i = 1; i<= n; i++){
+        console.log(" ".repeat(n-i) + "#".repeat(i))
+    }
+
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    staircase(n);
+}
