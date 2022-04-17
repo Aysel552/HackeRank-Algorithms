f5d2bd357c36005ed931a79b3ba3e01903626e05
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-04-17 23:11:23 +0400

    Day3 is solved

---
+++ 30DaysOfCode/Day3-Conditional Statements.js
@@ -0,0 +1,46 @@
+// Day3
+
+/*Given an integer, , perform the following conditional actions:
+
+    If n is odd, print Weird
+    If n is even and in the inclusive range of 2 to 5, print Not Weird
+    If n is even and in the inclusive range of 6 to 20, print Weird
+    If n is even and greater than 20, print Not Weird
+*/
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    var N = parseInt(readLine());
+    if (N%2 != 0 || (N > 5 && N < 21))
+        console.log("Weird");
+    else
+        console.log("Not Weird")
+
+}
+
+
+
+
